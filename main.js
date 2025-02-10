f2a6c194aeb8566d4d0e6bc586408be3a4b76ab2
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2025-02-09 20:10:20 -0500

    moved folders

---
+++ main.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+// import.meta.env.API_KEY
+
+const API_KEY = import.meta.env.VITE_APIKEY;
+
+const getExchangeRate = async (toCurrency, amt) => {
+    const allCurrencyData = await axios.get(`https://v6.exchangerate-api.com/v6/58c6070a273897eebcf58f36/latest/USD`)
+   
+    console.log(allCurrencyData)
+
+    const rate = allCurrencyData.data.conversion_rates;
+    const usd = 1 / rate["USD"]
+    const exchangeRate = usd * rate[toCurrency]
+    const finalAmount = amt * exchangeRate
+    output.textContent = finalAmount
+    
+  }
+
+const form = document.getElementById('my-form');
+const input = document.getElementById("amount-input");
+const countryName = document.getElementById("foreign_currency");
+const output = document.querySelector('#results');
+const button = document.querySelector('#btn');
+
+
+form.addEventListener("click", async function(event){
+    event.preventDefault();
+    const clickedVal = countryName.value;
+    const myInput = input.value; 
+    getExchangeRate(clickedVal, myInput);
+
+const reset = document.querySelectorAll(clickedVal, myInput);
+
+reset.forEach(input => {
+    if(input.value){
+        input.val = ""
+    }
+    else{
+        input.innerHTML = ""
+    }
+})
+
+});
+  
